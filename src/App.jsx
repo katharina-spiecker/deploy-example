d515fcd47d6e88f2cc3acf41bcd1f17af71cae0a
katharina-spiecker <kathispiecker@gmail.com>
2024-09-19 10:49:41 +0200

    deploy with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import './App.css'
+import { Route, Routes, NavLink } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      Test App 1
+    <nav>
+      <NavLink to="/">Startseite</NavLink>
+      <NavLink to="/about">Über uns</NavLink>
+      <NavLink to="/contact">Kontakt</NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
     </>
+  )
+}
+
+function Home() {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+function About() {
+  return (
+    <h1>About</h1>
+  )
+}
+
+function Contact() {
+  return (
+    <h1>Contact</h1>
   )
 }
 
